6d772b29fd04065f548682997082d4a665bb8eee
JeedHome44 <41892950+JeedHome44@users.noreply.github.com>
2023-06-13 10:58:29 +0200

    Update const.py

--- custom_components/comwatt_myenergy/const.py
+++ custom_components/comwatt_myenergy/const.py
@@ -1,1 +1,26 @@
+"""Constants for Comwatt My Energy."""
+from datetime import timedelta
 
+# Tools
+DEBUG_FLAG = False
+
+# Domain name
+DOMAIN = "comwatt_myenergy"
+
+# Config flow
+DATA_COORDINATOR = "coordinator"
+
+# Conf comwatt_site_code
+CONF_COMWATT_SITE_CODE = "comwatt_site_code"
+
+# Device name
+DEVICE_CONF_URL = "https://energy.comwatt.com/#/login"
+
+# Attribution
+ATTRIBUTION = "Data provided by Comwatt"
+
+# Scan interval (avoid synchronisation to lower request per seconds on server)
+LOGIN_STAT_SCAN_INTERVAL = timedelta(hours=1, minutes=30, seconds=13)
+
+# Max error theshold
+MAX_SERVER_ERROR_THRESHOLD = 5
