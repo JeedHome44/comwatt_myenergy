b8182d7380c5cd52b890ffd81bc65f4edd3215b8
JeedHome44 <41892950+JeedHome44@users.noreply.github.com>
2023-06-13 11:10:33 +0200

    Update config_flow.py

--- custom_components/comwatt_myenergy/config_flow.py
+++ custom_components/comwatt_myenergy/config_flow.py
@@ -1,1 +1,21 @@
+"""Define a config flow manager for Comwatt My Energy."""
+import logging
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME, CONF_PASSWORD, CONF_USERNAME
+
+# component library
+from .const import (
+    CONF_ATOME_SITE_CODE,
+    DOMAIN,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+COMWATT_MYENERGY_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+    }
+)
 
