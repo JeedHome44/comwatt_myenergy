3424c71ca111d3c5d7aeaa1376885e9a03c7dd75
JeedHome44 <41892950+JeedHome44@users.noreply.github.com>
2023-06-13 11:05:53 +0200

    Update __init__.py

--- custom_components/comwatt_myenergy/__init__.py
+++ custom_components/comwatt_myenergy/__init__.py
@@ -2,3 +2,13 @@
 import asyncio
 
 from .const import DATA_COORDINATOR, DOMAIN
+
+PLATFORMS = ["sensor"]
+
+DATA_LISTENER = "listener"
+
+
+async def async_setup(hass, config):
+    """Set up the Comwatt My Energy component."""
+    # hass.data[DOMAIN] = {DATA_COORDINATOR: {}, DATA_LISTENER: {}}
+    return True
