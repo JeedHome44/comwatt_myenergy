de566a44e15b8eba333b77713d9f0968d8bcae0e
Nicolas BENOIST <41892950+JeedHome44@users.noreply.github.com>
2023-06-18 19:30:24 +0200

    Update sensor.py

--- custom_components/comwatt_myenergy/sensor.py
+++ custom_components/comwatt_myenergy/sensor.py
@@ -1,1 +1,39 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
 
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import POWER_WATT
+from homeassistant.const import POWER_KILO_WATT
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor()])
+
+
+class ExampleSensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Example Power"
+    _attr_native_unit_of_measurement = POWER_WATT
+    _attr_device_class = SensorDeviceClass.POWER
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 0
