4369338bb5a3f6ec6d096eca37e0d356b364b042
Nicolas BENOIST <41892950+JeedHome44@users.noreply.github.com>
2023-06-22 01:21:35 +0200

    Update sensor.py

--- custom_components/comwatt_myenergy/sensor.py
+++ custom_components/comwatt_myenergy/sensor.py
@@ -9,31 +9,61 @@ from homeassistant.components.sensor import (
 from homeassistant.const import POWER_WATT
 from homeassistant.const import POWER_KILO_WATT
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
+SENSOR_TYPES = {
+    'current': ['Current', 'A'],
+    'power': ['Power', 'W'],
+    'voltage': ['Voltage', 'V'],
+}
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Configurer les capteurs de Comwatt My Energy."""
+    data = hass.data[DOMAIN][config_entry.entry_id]
+    sensors = []
 
+    for sensor_type in SENSOR_TYPES:
+        name = SENSOR_TYPES[sensor_type][0]
+        unit = SENSOR_TYPES[sensor_type][1]
+        sensors.append(ComwattMyEnergySensor(data, sensor_type, name, unit))
 
-class ExampleSensor(SensorEntity):
-    """Representation of a Sensor."""
+    async_add_entities(sensors)
 
-    _attr_name = "Example Power"
-    _attr_native_unit_of_measurement = POWER_WATT
-    _attr_device_class = SensorDeviceClass.POWER
-    _attr_state_class = SensorStateClass.MEASUREMENT
+class ComwattMyEnergySensor(Entity):
+    """Capteur pour la box Comwatt My Energy."""
 
-    def update(self) -> None:
-        """Fetch new state data for the sensor.
+    def __init__(self, data, sensor_type, name, unit):
+        """Initialiser le capteur."""
+        self._data = data
+        self._sensor_type = sensor_type
+        self._name = name
+        self._unit = unit
+        self._state = None
 
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._attr_native_value = 0
+    @property
+    def name(self):
+        """Nom du capteur."""
+        return f"Comwatt My Energy {self._name}"
+
+    @property
+    def unique_id(self):
+        """ID unique du capteur."""
+        return f"comwatt_myenergy_{self._sensor_type}"
+
+    @property
+    def unit_of_measurement(self):
+        """Unité de mesure du capteur."""
+        return self._unit
+
+    @property
+    def state(self):
+        """Valeur du capteur."""
+        return self._state
+
+    async def async_update(self):
+        """Mettre à jour la valeur du capteur."""
+        # Utilisez les fonctions ou les API nécessaires pour récupérer les données de la box Comwatt Gen4
+        # et mettez à jour self._state avec la valeur appropriée
+        self._state = 42  # Exemple de valeur statique pour illustrer
